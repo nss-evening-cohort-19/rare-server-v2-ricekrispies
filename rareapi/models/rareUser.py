a269b92c6ad934e28d65176a40497eafe9d5d9cf
Keaton Law <keaton.c.law@gmail.com>
2022-12-12 19:32:42 -0600

    moar stuff

--- rareapi/models/rareUser.py
+++ rareapi/models/rareUser.py
@@ -4,5 +4,9 @@ class RareUser(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     bio = models.CharField(max_length=250)
+    email = models.CharField(max_length=50)
+    created_on = models.DateField()
+    active = models.BooleanField()
+    is_staff = models.BooleanField()
     profile_image_url = models.ImageField('profileimages', height_field=None,
         width_field=None, max_length=None, null=True)
