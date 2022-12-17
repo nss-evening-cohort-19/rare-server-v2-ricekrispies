c98c0e0bd205c209469668a3b4ec13e4fb0cd26b
Keaton Law <keaton.c.law@gmail.com>
2022-12-17 09:51:30 -0600

    added actions to change is_staff and active

--- rareapi/views/__init__.py
+++ rareapi/views/__init__.py
@@ -2,4 +2,4 @@ from .auth import check_user, register_user
 from .rare_user import RareUserView
 from .post import PostView
 from .user_change import UserChangeView
-from .rare_user import UserView
+from .rare_user import RareUserView
