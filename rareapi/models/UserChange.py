68fa62d88fcab503244bea13be1f801dcbdb905c
Christian Black <blackl3@gmail.com>
2022-12-21 20:27:04 -0600

    changed user_change model to allow for null values

--- rareapi/models/UserChange.py
+++ rareapi/models/UserChange.py
@@ -3,5 +3,6 @@ from django.db import models
 class UserChange(models.Model):
     action = models.CharField(max_length=25)
     admin = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name='admin')
-    second_admin = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name='second_admin')
+    second_admin = models.ForeignKey(
+        "RareUser", on_delete=models.CASCADE, related_name='second_admin', null=True, default=None)
     modified_user = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name='modified_user')
